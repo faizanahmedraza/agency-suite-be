688d7537580bdaf0cd62f43b27d8a47a3f02ca20
Mehmood97 <mehmood.ismail@saasfa.com>
2022-03-14 19:55:04 +0500

    agency object work done

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,6 +13,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+
+        $this->app->singleton('agency', function () {
+            $request = app(\Illuminate\Http\Request::class);
+            $agencyDomain = \App\Models\AgencyDomain::where('domain',$request->getHost())->first();
+
+            $agency = new \stdClass;
+            if($agencyDomain){
+                $agency->id = $agencyDomain->agency_id;
+                $agency->domain_name = $agencyDomain->domain;
+                $agency->name=optional($agencyDomain->agency)->name;
+            }
+            return $agency;
+        });
     }
 }
