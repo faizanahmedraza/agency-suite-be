25292adefaef2b1062cff59d8857867a7048fc4a
unknown <faizan.raza@saasfa.com>
2022-03-15 19:09:33 +0500

    add changes

--- app/Http/Requests/V1/Agency/RoleRequest.php
+++ app/Http/Requests/V1/Agency/RoleRequest.php
@@ -41,7 +41,7 @@ class RoleRequest extends RequestAbstract
     public function rules(): array
     {
         return [
-            'name' => $this->isMethod('PUT') ? "required|string|max:255|unique:roles,name," . $this->id.',id,agency_id,'.app('agency')->id : "required|string|max:255|unique:roles,name,NULL,id,agency_id,".app('agency')->id,
+            'name' => $this->isMethod('PUT') ? "required|string|max:255|regex:/^[A-Za-z0-9_-]+[A-Za-z0-9]$/i|unique:roles,name," . $this->id.',id,agency_id,'.app('agency')->id : "required|string|max:255|regex:/^[A-Za-z0-9_-]+[A-Za-z0-9]$/i|unique:roles,name,NULL,id,agency_id,".app('agency')->id,
             'permissions' => 'sometimes|nullable|array',
             'permissions.*' => 'sometimes|nullable|in:' . implode(',', Permission::where('name', 'like', Role::ROLES_PREFIXES['agency'].'%')->pluck('id')->toArray())
         ];
