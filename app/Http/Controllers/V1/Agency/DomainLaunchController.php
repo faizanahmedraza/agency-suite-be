b5be18ccf3b103023a9fd814f5e1e30801e5e788
Faizan Ahmed Raza <faizan.raza@saasfa.com>
2022-04-13 14:59:00 +0500

    doc changes

--- app/Http/Controllers/V1/Agency/DomainLaunchController.php
+++ app/Http/Controllers/V1/Agency/DomainLaunchController.php
@@ -10,10 +10,18 @@ use App\Http\Services\V1\Agency\AgencyDomainService;
 
 /**
  * @group Agency Domain Launch
- * @authenticated
  */
 class DomainLaunchController extends Controller
 {
+    /**
+     * Public Agency Domain Launch Api
+     * This api to verify the domain before jump to agency portal.
+     *
+     * @bodyParam domain string required
+     *
+     * @responseFile 422 responses/ValidationResponse.json
+     * @responseFile 200 responses/V1/Agency/PortalSettingResponse.json
+     */
     public function index(DomainLaunchRequest $request)
     {
         $resp = AgencyDomainService::first(['domain' => $request->domain]);
