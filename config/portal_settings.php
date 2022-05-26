059ab01553886ead6957baf931910dafa11fefbc
Faizan Ahmed Raza <faizan.raza@saasfa.com>
2022-05-26 12:04:50 +0500

    add changeS

--- config/portal_settings.php
+++ config/portal_settings.php
@@ -3,7 +3,7 @@
 return [
     'name' => env('APP_NAME','Agency Tool'),
     'primary_color' => env('PRIMARY_COLOR','#283046'),
-    'secondary_color' => env('SECONDARY_COLOR','#fff'),
+    'secondary_color' => env('SECONDARY_COLOR','#FFFFFF'),
     'logo' => env('LOGO','https://res.cloudinary.com/saasfa/image/upload/v1650350502/service-625e59a64167b.jpg'),
     'favicon' => env('FAVICON','https://res.cloudinary.com/saasfa/image/upload/v1650437747/service-625fae73a70da.ico')
 ];
