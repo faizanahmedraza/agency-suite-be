a7e47e69bb82c40265c840ced6ea26800316c23e
Faizan Ahmed Raza <faizan.raza@saasfa.com>
2022-05-26 19:13:08 +0500

    commit

--- app/Http/Businesses/V1/Customer/BillingInformationBusiness.php
+++ app/Http/Businesses/V1/Customer/BillingInformationBusiness.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Businesses\V1\Customer;
 
+use App\Exceptions\V1\RequestValidationException;
 use App\Http\Services\V1\Customer\BillingInformationService;
+use Carbon\Carbon;
 
 class BillingInformationBusiness
 {
     public static function store($request)
     {
+        $date = Carbon::now();
+        $month = $date->format('m');
+        $year = $date->format('y');
+        if ($request->expiry_month < $month && $request->expiry_year < $year) {
+            throw RequestValidationException::errorMessage("Expiry date cann't be less than current date.");
+        }
         return BillingInformationService::store($request);
     }
 
@@ -18,10 +26,10 @@ class BillingInformationBusiness
 
     public static function first($id)
     {
-        return BillingInformationService::first($id,['agency','customer']);
+        return BillingInformationService::first($id, ['agency', 'customer']);
     }
 
-    public static function update($request,$id)
+    public static function update($request, $id)
     {
         $billing = self::first($id);
         return BillingInformationService::update($request, $billing);
